42a5feced14e7917b73186a04050354966acd7de
Troy Feng <troy.feng@yale.edu>
2021-06-17 23:41:35 -0400

    Add multi-doc summarization unittests

--- tests/model_test.py
+++ tests/model_test.py
@@ -5,6 +5,8 @@ from dataset.huggingface_datasets import CnndmDataset
 from model import SUPPORTED_SUMM_MODELS, list_all_models
 from model.lexrank_model import LexRankModel
 from model.longformer_model import LongformerModel
+from model.multi_doc_joint_model import MultiDocJointModel
+from model.multi_doc_separate_model import MultiDocSeparateModel
 
 
 class TestModels(unittest.TestCase):
@@ -20,7 +22,7 @@ class TestModels(unittest.TestCase):
 
         src, tgt = zip(*(list(map(lambda x: (x.source, x.summary), subset))))
 
-        return src, tgt
+        return list(src), list(tgt)
 
     def test_list_models(self):
         print(f"{'#'*10} test_list_models STARTS {'#'*10}")
@@ -35,7 +37,7 @@ class TestModels(unittest.TestCase):
         print(f"{'#'*10} test_model_summarize STARTS {'#'*10}")
         all_models = list_all_models()
         for model_class, _ in all_models:
-            print(f"Test on {model_class}")
+            print(f"\nTest on {model_class}")
 
             if model_class == LexRankModel:
                 # current LexRankModel requires a training set
@@ -44,11 +46,16 @@ class TestModels(unittest.TestCase):
             else:
                 model = model_class()
 
-            test_src, test_tgt = self.get_summarization_set(1)
-            prediction = model.summarize(test_src[0] * 5 if model_class == LongformerModel else test_src)
-            print(
-                f"Gold summary: {test_tgt} \nPredicted summary: {prediction}")
-        print(f"{'#'*10} test_model_summarize ENDS {'#'*10}\n")
+            if isinstance(model, (MultiDocJointModel, MultiDocSeparateModel)):
+                test_src, test_tgt = self.get_summarization_set(3)
+                prediction = model.summarize([test_src])
+                print(f"Gold summary: {test_tgt} \nPredicted summary: {prediction}")
+            else:
+                test_src, test_tgt = self.get_summarization_set(1)
+                prediction = model.summarize([test_src[0] * 5] if model_class == LongformerModel else test_src)
+                print(f"Gold summary: {test_tgt} \nPredicted summary: {prediction}")
+
+        print(f"{'#'*10} test_model_summarize ENDS {'#'*10}\n\n")
 
 
 if __name__ == '__main__':
