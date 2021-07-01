2c0591edfc58d243e27be9caac2d74728d552c78
Troy Feng <troy.feng@yale.edu>
2021-06-30 22:50:17 -0400

    Clean up logs + initialization

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -5,7 +5,7 @@ from model.base_model import SummModel
 from evaluation import SUPPORTED_EVALUATION_METRICS
 from evaluation.base_metric import SummMetric
 from dataset import SUPPORTED_SUMM_DATASETS
-from dataset.st_dataset import SummInstance
+from dataset.st_dataset import SummInstance, SummDataset
 
 import random
 import time
@@ -17,13 +17,16 @@ from model.single_doc import LexRankModel
 from model.multi_doc import MultiDocJointModel
 from model.multi_doc import MultiDocSeparateModel
 from dataset.non_huggingface_datasets import ScisummnetDataset
+from dataset.huggingface_datasets import CnndmDataset
 
 class IntegrationTests(unittest.TestCase):
     @staticmethod
+    def print_with_color(s: str, color: str):
+        print(f"\033[{color}m{s}\033[0m")
+
+    @staticmethod
     def flatten_list_to_str(doc: Union[str, List[str]]) -> str:
-        if isinstance(doc, list):
-            return " ".join(doc)
-        return doc
+        return " ".join(doc) if isinstance(doc, list) else doc
 
     def retrieve_test_instances(self, dataset_instances: List[SummInstance], num_instances = 5) -> List[SummInstance]:
         test_instances = []
@@ -31,30 +34,49 @@ class IntegrationTests(unittest.TestCase):
             test_instances.append(dataset_instances[random.randint(0, len(dataset_instances) - 1)])
         return test_instances
 
-    def test_single_integration(self, dataset_class, dataset_instances: List[SummInstance], model_class, model_instance: SummModel, evaluation_metric: SummMetric):
-        print(f"\n{'#' * 20} TESTING: dataset {dataset_class}, model {model_instance.model_name}, evaluation metric {evaluation_metric.metric_name} {'#' * 20}")
-        src = [ins.source[0] for ins in dataset_instances] if dataset_class == ScisummnetDataset else [IntegrationTests.flatten_list_to_str(ins.source) for ins in dataset_instances]
-        tgt = [ins.summary for ins in dataset_instances]
-        prediction = model_instance.summarize([src] if model_class == MultiDocJointModel or model_class == MultiDocSeparateModel else src)
+    def _test_single_integration(self, dataset: SummDataset, test_instances: List[SummInstance], model: SummModel, metric: SummMetric):
+        IntegrationTests.print_with_color(f"{'#' * 20} Testing: {dataset.dataset_name} dataset, {model.model_name} model, {metric.metric_name} evaluation metric {'#' * 20}", "35")
+
+        src = [ins.source[0] for ins in test_instances] if isinstance(dataset, ScisummnetDataset) else [IntegrationTests.flatten_list_to_str(ins.source) for ins in test_instances]
+        tgt = [ins.summary for ins in test_instances]
+        prediction = model.summarize([src] if isinstance(model, (MultiDocJointModel, MultiDocSeparateModel)) else src)
         print(prediction)
         print(tgt)
-        score_dict = evaluation_metric.evaluate(prediction, [" ".join(tgt)] if model_class == MultiDocJointModel or model_class == MultiDocSeparateModel else tgt)
+        score_dict = metric.evaluate(prediction, [" ".join(tgt)] if isinstance(model, (MultiDocJointModel, MultiDocSeparateModel)) else tgt)
         print(score_dict)
-        return
 
     def test_all(self):
-        for dataset_class in SUPPORTED_SUMM_DATASETS:
-            dataset = dataset_class()
+        IntegrationTests.print_with_color("Initializing all datasets...", "35")
+        # datasets = []
+        # for dataset_cls in SUPPORTED_SUMM_DATASETS:
+        #     print(dataset_cls)
+        #     ds = dataset_cls()
+        #     datasets.append(ds)
+        lxr_dataset = CnndmDataset()
+        IntegrationTests.print_with_color("Initializing all models...", "35")
+        models = []
+        for model_cls in SUPPORTED_SUMM_MODELS:
+            print(model_cls)
+            models.append(model_cls([x.source for x in list(lxr_dataset.train_set)[0:100]]) if model_cls == LexRankModel else model_cls())
+        IntegrationTests.print_with_color("Initializing all evaluation metrics...", "35")
+        evaluation_metrics = []
+        for eval_cls in SUPPORTED_EVALUATION_METRICS:
+            if eval_cls != Rouge and eval_cls != RougeWe:
+                print(eval_cls)
+                evaluation_metrics.append(eval_cls())
+
+        print('\n\n')
+        IntegrationTests.print_with_color("Beginning integration tests...", "32")
+        for dataset_cls in SUPPORTED_SUMM_DATASETS:
+            dataset = dataset_cls()
             if dataset.train_set is not None:
-                train_set = list(dataset.train_set)
-                print(f"{dataset_class} has a training set of {len(train_set)} examples")
-                dataset_instances = self.retrieve_test_instances(train_set)
-                for model_class in SUPPORTED_SUMM_MODELS:
-                    model = model_class([IntegrationTests.flatten_list_to_str(x.source[0] if dataset_class == ScisummnetDataset else x.source) for x in train_set[0:100]]) if model_class == LexRankModel else model_class()
-                    for eval_class in SUPPORTED_EVALUATION_METRICS:
-                        if eval_class != Rouge and eval_class != RougeWe:
-                            eval_metric = eval_class()
-                            self.test_single_integration(dataset_class = dataset_class, dataset_instances = dataset_instances, model_class = model_class, model_instance = model, evaluation_metric = eval_metric)
+                dataset_instances = list(dataset.train_set)
+                print(f"{dataset.dataset_name} has a training set of {len(dataset_instances)} examples")
+                test_instances = self.retrieve_test_instances(dataset_instances)
+                for model in models:
+                    for metric in evaluation_metrics:
+                        print('\n')
+                        self._test_single_integration(dataset = dataset, test_instances = test_instances, model = model, metric = metric)
 
 if __name__ == '__main__':
     random.seed(time.time())
