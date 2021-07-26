74505146905bf24d9cb9baae613bd487db5c0ec5
Troy Feng <troy.feng@yale.edu>
2021-07-26 19:59:41 -0400

    Formatting

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -8,11 +8,14 @@ from typing import List
 
 def assemble_model_pipeline(dataset: SummDataset) -> List[SummModel]:
     """
-    Returns filtered subset of `SUPPORTED_SUMM_MODELS` where the summarization task matches given `dataset`.
+    Returns filtered subset of `SUPPORTED_SUMM_MODELS` where the summarization
+    task matches given `dataset`.
     """
-    return list(filter(
-        lambda model:
-            model.is_dialogue_based == dataset.is_dialogue_based and
-            model.is_multi_document == dataset.is_multi_document and
-            model.is_query_based == dataset.is_query_based,
-        SUPPORTED_SUMM_MODELS))
+    return list(
+        filter(
+            lambda model: model.is_dialogue_based == dataset.is_dialogue_based
+            and model.is_multi_document == dataset.is_multi_document
+            and model.is_query_based == dataset.is_query_based,
+            SUPPORTED_SUMM_MODELS,
+        )
+    )
