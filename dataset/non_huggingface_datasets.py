6fc93f3d86cb4dc2b2b0162adad846ac5aedafff
Mutuma Murori <mmm274@zoo.cs.yale.edu>
2021-07-28 13:24:36 -0400

    Implements caching system for non-huggingface datasets. Datasets implemented onScisummnet, Summscreen and QMsum

--- dataset/non_huggingface_datasets.py
+++ dataset/non_huggingface_datasets.py
@@ -6,7 +6,16 @@ import random
 import gdown
 from urllib.request import urlretrieve, urlopen
 from typing import Optional, List, Tuple
+# from st_dataset import SummInstance, SummDataset
 from dataset.st_dataset import SummInstance, SummDataset
+
+import ast
+import datasets
+from datasets import Dataset
+from tqdm import tqdm
+
+
+BASE_NONHUGGINGFACE_DATASETS_PATH = os.path.join(os.getcwd(), "dataset", "non_huggingface_datasets_builders")
 
 
 class ScisummnetDataset(SummDataset):
@@ -15,63 +24,38 @@ class ScisummnetDataset(SummDataset):
         information for the dataset
     """
     
-    download_link = "https://cs.stanford.edu/~myasu/projects/scisumm_net/scisummnet_release1.1__20190413.zip"
+    dataset_name = "ScisummNet"
+    version = "1.1.0"
+    description = "A summary of scientific papers should ideally incorporate the impact of the papers on the " \
+                    "research community reflected by citations. To facilitate research in citation-aware scientific " \
+                    "paper summarization (Scisumm), the CL-Scisumm shared task has been organized since 2014 for " \
+                    "papers in the computational linguistics and NLP domain."
+
+                    
+    builder_script_path = os.path.join(BASE_NONHUGGINGFACE_DATASETS_PATH, dataset_name.lower() + ".py")
     
-    def __init__(self):
-    
-        # Download and unzip the dataset in the temp directory
-        tmp_dir = tempfile.TemporaryDirectory()
-        zip_path, _ = urlretrieve(ScisummnetDataset.download_link)
-        with zipfile.ZipFile(zip_path, "r") as f:
-            f.extractall(tmp_dir.name)
-            
-        # Read from individual folders for the article and the summary
-        extraction_path = os.path.join(tmp_dir.name, 'scisummnet_release1.1__20190413', 'top1000_complete')
-
-        # Extract the dataset entries from folders and load into dataset
-        scisumm_ds = []
-        for folder in os.listdir(extraction_path):
-            entry = {}
-            entry['entry_number'] = folder
-
-            doc_xml_path = os.path.join(extraction_path, folder, 'Documents_xml', folder + ".xml")
-            with open (doc_xml_path, "r", encoding='utf-8') as f:
-                entry['document_xml'] = f.read()
-
-            cite_annot_path = os.path.join(extraction_path, folder, 'citing_sentences_annotated.json')
-            with open (cite_annot_path, "r", encoding='utf-8') as f:
-                entry['citing_sentences_annotated.json'] = json.load(f)
-
-            summary_path = os.path.join(extraction_path, folder, 'summary', folder + ".gold.txt")
-            with open (summary_path, "r", encoding='utf-8') as f:
-                entry['summary'] = f.read()
-
-            # Create a Summarization instance for every unit and add to the dataset
-            summ_entry_instance = SummInstance(source = [entry['document_xml'], entry['citing_sentences_annotated.json']],\
-                                               summary = entry['summary'])
-            scisumm_ds.append(summ_entry_instance)
-
-        tmp_dir.cleanup()
+    def __init__(self):   
 
 
-        # Randomize and split data into train, dev and test sets
-        random.shuffle(scisumm_ds)
-        split_1 = int(0.8 * len(scisumm_ds))
-        split_2 = int(0.9 * len(scisumm_ds))
-
-        processed_train_set = scisumm_ds[:split_1]
-        processed_dev_set = scisumm_ds[split_1:split_2]
-        processed_test_set = scisumm_ds[split_2:]
-
+        # Load dataset
+        scisummnet_dataset = datasets.load_dataset(path=ScisummnetDataset.builder_script_path)
+        info_set = scisummnet_dataset["train"]
         
-        #  Process the train, dev and test set and replace the last three args in __init__() below
-        dataset_name = "ScisummNet_v1.1"
-        description = "A summary of scientific papers should ideally incorporate the impact of the papers on the " \
-                      "research community reflected by citations. To facilitate research in citation-aware scientific " \
-                      "paper summarization (Scisumm), the CL-Scisumm shared task has been organized since 2014 for " \
-                      "papers in the computational linguistics and NLP domain."
-        super().__init__(dataset_name,
-                         description,
+        #  Process the train, dev and test se
+        processed_dataset = ScisummnetDataset.process_scisummnet_data(scisummnet_dataset["train"])
+        
+        # Randomize and split data into train, dev and test sets
+        processed_dataset = list(processed_dataset)
+        random.shuffle(processed_dataset)
+        split_1 = int(0.8 * len(processed_dataset))
+        split_2 = int(0.9 * len(processed_dataset))
+
+        processed_train_set = processed_dataset[:split_1]
+        processed_dev_set = processed_dataset[split_1:split_2]
+        processed_test_set = processed_dataset[split_2:]
+
+        super().__init__(ScisummnetDataset.dataset_name,
+                         ScisummnetDataset.description,
                          is_dialogue_based=False,
                          is_multi_document=False,
                          is_query_based=False,
@@ -79,7 +63,15 @@ class ScisummnetDataset(SummDataset):
                          dev_set=processed_dev_set, 
                          test_set=processed_test_set, 
                          )
-        
+
+    @staticmethod
+    def process_scisummnet_data(data: Dataset) -> List[SummInstance]:
+        for instance in tqdm(data):
+            docs: List = [instance['document_xml'], instance['citing_sentences_annotated.json']]
+            summary: str = instance['summary']
+            summ_instance = SummInstance(source=docs, summary=summary)
+            
+            yield summ_instance
 
 
 class SummscreenDataset(SummDataset):
@@ -87,35 +79,22 @@ class SummscreenDataset(SummDataset):
     The SummScreen dataset. As a dataset not included by huggingface, we need to do manually download, set basic
         information for the dataset
     """
-    
-    download_link = 'https://drive.google.com/uc?id=1BvdIllGBo9d2-bzXQRzWuJXB04XPVmfF'
+      
+    dataset_name = "Summscreen"
+    version = "1.1.0"
+
+    builder_script_path = os.path.join(BASE_NONHUGGINGFACE_DATASETS_PATH, dataset_name.lower() + ".py")
     
     def __init__(self):
-    
         
-        # download and unzip the dataset in the temp directory
-        tmp_dir = tempfile.TemporaryDirectory()
-
-        zip_path = os.path.join(tmp_dir.name, 'sumscreen.zip')
-        gdown.download(SummscreenDataset.download_link, zip_path, quiet=False) 
-
-        with zipfile.ZipFile(zip_path, "r") as f:
-            f.extractall(tmp_dir.name)
-
-        # Read from individual folders for the article and the summary
-        extraction_path = os.path.join(tmp_dir.name, 'SummScreen')
-
-
-        # Extract the dataset entries from folders and load into dataset
-        processed_train_set = SummscreenDataset.process_summscreen_data(os.path.join(extraction_path, 'ForeverDreaming', 'fd_train.json')) 
-        processed_train_set += SummscreenDataset.process_summscreen_data(os.path.join(extraction_path, 'TVMegaSite', 'tms_train.json'))
-        processed_dev_set = SummscreenDataset.process_summscreen_data(os.path.join(extraction_path, 'ForeverDreaming', 'fd_dev.json')) 
-        processed_dev_set += SummscreenDataset.process_summscreen_data(os.path.join(extraction_path, 'TVMegaSite', 'tms_dev.json'))
-        processed_test_set = SummscreenDataset.process_summscreen_data(os.path.join(extraction_path, 'ForeverDreaming', 'fd_test.json')) 
-        processed_test_set += SummscreenDataset.process_summscreen_data(os.path.join(extraction_path, 'TVMegaSite', 'tms_test.json'))
-
-        tmp_dir.cleanup()
-
+        # Load dataset
+        summscreen_dataset = datasets.load_dataset(path=SummscreenDataset.builder_script_path)
+        info_set = summscreen_dataset["train"]
+        
+        #  Process the train, dev and test se
+        processed_train_set = SummscreenDataset.process_summscreen_data(summscreen_dataset["train"])
+        processed_dev_set = SummscreenDataset.process_summscreen_data(summscreen_dataset["validation"]) 
+        processed_test_set = SummscreenDataset.process_summscreen_data(summscreen_dataset["test"])
         
         #  Process the train, dev and test set and replace the last three args in __init__() below
         dataset_name = "SummScreen_fd+tms_tokenized"
@@ -132,26 +111,16 @@ class SummscreenDataset(SummDataset):
                          dev_set=processed_dev_set, 
                          test_set=processed_test_set, 
                          )
-
+    
     @staticmethod
-    def process_summscreen_data(file_path: str) -> List[SummInstance]:
-
-        entries_set = []
-
-        infile = open(file_path, 'r')
-        for line in infile:
-            processed_line = line.replace("@@ ", "")
-            data = json.loads(processed_line)
-            entries_set.append(data)
-        infile.close()
-
+    def process_summscreen_data(data: Dataset) -> List[SummInstance]:
         processed_set = []
-        for instance in entries_set:
-            transcript: List = instance['Transcript']
-            recap: str = instance['Recap'][0]               # Recap is a single string in list
+        for instance in tqdm(data):
+            transcript: List = instance['transcript']   #convert string into a list of string dialogues
+            recap: str = instance['recap']
             summ_instance = SummInstance(source=transcript, summary=recap)
             processed_set.append(summ_instance)
-
+            
         return processed_set
 
 
@@ -159,26 +128,33 @@ class SummscreenDataset(SummDataset):
 
 class QMsumDataset(SummDataset):
     """
-    QMSum is a new human-annotated benchmark for query-based multi-domain meeting summarization task, \
-        which consists of 1,808 query-summary pairs over 232 meetings in multiple domains.
+    QMSum Dataset
     """
-    
-    download_link = "https://raw.githubusercontent.com/Yale-LILY/QMSum/main/data/ALL/jsonl/"
+
+    dataset_name = "QMsum"
+    description = '''
+    QMSum is a new human-annotated benchmark for query-based multi-domain meeting summarization task,
+    which consists of 1,808 query-summary pairs over 232 meetings in multiple domains.
+    '''
+    version = "1.0.0"
+
+    builder_script_path = os.path.join(BASE_NONHUGGINGFACE_DATASETS_PATH, dataset_name.lower() + ".py")
     
     def __init__(self):
+        
+        # Load dataset
+        qmsum_dataset = datasets.load_dataset(path=QMsumDataset.builder_script_path)
+        info_set = qmsum_dataset["train"]
 
         # Extract the dataset entries from folders and load into dataset
-        processed_train_set = QMsumDataset.process_qmsum_data("train")
-        processed_dev_set = QMsumDataset.process_qmsum_data("val")
-        processed_test_set = QMsumDataset.process_qmsum_data("test")
+        processed_train_set = QMsumDataset.process_qmsum_data(qmsum_dataset["train"])
+        processed_dev_set = QMsumDataset.process_qmsum_data(qmsum_dataset["validation"])
+        processed_test_set = QMsumDataset.process_qmsum_data(qmsum_dataset["test"])
 
         
         #  Process the train, dev and test set and replace the last three args in __init__() below
-        dataset_name = "QMsum"
-        description = "QMSum is a new human-annotated benchmark for query-based multi-domain meeting summarization task, \
-                        which consists of 1,808 query-summary pairs over 232 meetings in multiple domains."
-        super().__init__(dataset_name,
-                         description,
+        super().__init__(QMsumDataset.dataset_name,
+                         QMsumDataset.description,
                          is_dialogue_based=True,
                          is_multi_document=False,
                          is_query_based=True,
@@ -189,16 +165,9 @@ class QMsumDataset(SummDataset):
         
 
     @staticmethod
-    def process_qmsum_data(split: str) -> List[SummInstance]:
-
-        data_path, _ = urlretrieve(QMsumDataset.download_link + split + ".jsonl")
-        data = []
-        with open(data_path) as f:
-            for line in f:
-                data.append(json.loads(line))
-
+    def process_qmsum_data(data: Dataset) -> List[SummInstance]:
         processed_set = []
-        for instance in data:
+        for instance in tqdm(data):
             for query_set in instance['general_query_list'] + instance['specific_query_list']:
                 meeting: List = [utterance['speaker'] + " : " + utterance['content']\
                                 for utterance in instance['meeting_transcripts']]
@@ -206,6 +175,7 @@ class QMsumDataset(SummDataset):
                 summary: str = query_set['answer']    
                 summ_instance = SummInstance(source=meeting, summary=summary, query=query)
                 processed_set.append(summ_instance)
+
 
         return processed_set
 
---
+++ dataset/non_huggingface_datasets_builders/summscreen.py
@@ -0,0 +1,128 @@
+import os
+import json
+import datasets
+
+
+"""Summscreen dataset."""
+
+
+_CITATION = """
+@article{DBLP:journals/corr/abs-2104-07091,
+  author    = {Mingda Chen and
+               Zewei Chu and
+               Sam Wiseman and
+               Kevin Gimpel},
+  title     = {SummScreen: {A} Dataset for Abstractive Screenplay Summarization},
+  journal   = {CoRR},
+  volume    = {abs/2104.07091},
+  year      = {2021},
+  url       = {https://arxiv.org/abs/2104.07091},
+  archivePrefix = {arXiv},
+  eprint    = {2104.07091},
+  timestamp = {Mon, 19 Apr 2021 16:45:47 +0200},
+  biburl    = {https://dblp.org/rec/journals/corr/abs-2104-07091.bib},
+  bibsource = {dblp computer science bibliography, https://dblp.org}
+}
+"""
+
+_DESCRIPTION = """
+A summary of scientific papers should ideally incorporate the impact of the papers on the research community 
+reflected by citations. To facilitate research in citation-aware scientific paper summarization (Scisumm), 
+the CL-Scisumm shared task has been organized since 2014 for papers in the computational linguistics and NLP domain.
+"""
+
+_HOMEPAGE = "https://github.com/mingdachen/SummScreen"
+
+_LICENSE = "MIT Licencse"
+
+_URLs = 'https://drive.google.com/uc?id=1BvdIllGBo9d2-bzXQRzWuJXB04XPVmfF'
+
+
+
+
+class SummertimeScisummnet(datasets.GeneratorBasedBuilder):
+    """Scisummnet dataset."""
+
+    VERSION = datasets.Version("1.1.0")
+
+    BUILDER_CONFIGS = [
+        datasets.BuilderConfig(),
+    ]
+
+    def _info(self):
+        features = datasets.Features(
+            {
+                'entry_number': datasets.Value("int64"),
+                'transcript': datasets.features.Sequence(datasets.Value("string")),
+                'recap': datasets.Value("string")
+            }
+        )
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=features,
+            supervised_keys=None,
+            homepage=_HOMEPAGE,
+            license=_LICENSE,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+        my_urls = _URLs
+        path = dl_manager.download_and_extract(my_urls)
+        path = os.path.join(path, "SummScreen")
+
+        trainpath_fd = os.path.join('ForeverDreaming', 'fd_train.json')
+        trainpath_tms = os.path.join('TVMegaSite', 'tms_train.json')
+        trainpaths = [trainpath_fd, trainpath_tms]
+
+        devpath_fd = os.path.join('ForeverDreaming', 'fd_dev.json')
+        devpath_tms = os.path.join('TVMegaSite', 'tms_dev.json')
+        devpaths = [devpath_fd, devpath_tms]
+
+        testpath_fd = os.path.join('ForeverDreaming', 'fd_test.json')
+        testpath_tms = os.path.join('TVMegaSite', 'tms_test.json')
+        testpaths = [testpath_fd, testpath_tms]
+
+
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs = {"filepaths": (path, trainpaths), "split": "train"}
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.VALIDATION,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs = {"filepaths": (path, devpaths), "split": "dev"}
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs = {"filepaths": (path, testpaths), "split": "test"}
+            )
+        ]
+
+    def _generate_examples(self, filepaths, split):
+        """Yields examples."""
+
+        counter = 0
+        path, relative_filepaths = filepaths
+        for filepath in relative_filepaths:
+
+            extraction_path = os.path.join(path, filepath)
+
+            with open(extraction_path, 'r') as f:
+                for line in f:
+                    processed_line = line.replace("@@ ", "")
+                    instance = json.loads(processed_line)
+
+                    entry = {}
+                    entry['entry_number'] = counter
+                    counter+=1
+
+                    entry['transcript'] = instance['Transcript']
+                    entry['recap'] = instance['Recap'][0]    # Recap is a single string in list
+
+                    yield entry['entry_number'], entry
+        
