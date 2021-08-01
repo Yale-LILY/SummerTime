7deaac19ec7e7aef04a877e2d370ac208111fa50
Murori <donmurori@gmail.com>
2021-08-01 13:36:53 -0400

    Cuts down on unnecessary imports

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -1,7 +1,6 @@
 import unittest
-from typing import Tuple, List, Optional
 
-from dataset import SUPPORTED_SUMM_DATASETS, list_all_datasets
+from dataset import SUPPORTED_SUMM_DATASETS
 from dataset.st_dataset import SummDataset, SummInstance
 
 
