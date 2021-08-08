f196a26c938d0e498aea3ebbf8740fee6b424d88
Troy Feng <troy.feng@yale.edu>
2021-08-08 13:16:28 -0400

    Clean up datasets tests

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -1,9 +1,12 @@
 import unittest
 from typing import Tuple, List, Optional
 
-from dataset.huggingface_datasets import CnndmDataset, MultinewsDataset
+from dataset.huggingface_datasets import MlsumDataset
+from dataset.non_huggingface_datasets import ArxivDataset
 from dataset import SUPPORTED_SUMM_DATASETS, list_all_datasets
 from dataset.st_dataset import SummDataset, SummInstance
+
+from helpers import print_with_color
 
 
 class TestDatasets(unittest.TestCase):
@@ -18,10 +21,14 @@ class TestDatasets(unittest.TestCase):
             self.assertTrue(isinstance(ins.query, str))
 
     def test_all_datasets(self):
-        print(f"{'#' * 10} test_all_datasets STARTS {'#' * 10}")
+        print_with_color(f"{'#' * 10} Testing all datasets... {'#' * 10}\n\n", "35")
 
         for ds_cls in SUPPORTED_SUMM_DATASETS:
-            print(f"Testing on the {ds_cls} dataset...")
+            # Skip MLSumm (Gitlab: server-side login gating) and Arxiv (size/time)
+            if ds_cls in [MlsumDataset, ArxivDataset]:
+                continue
+
+            print_with_color(f"Testing {ds_cls} dataset...", "35")
             ds: SummDataset = ds_cls()
 
             # must have at least one of train/dev/test set
@@ -41,8 +48,10 @@ class TestDatasets(unittest.TestCase):
                 test_set = list(ds.test_set)
                 print(f"{ds_cls} has a training set of {len(test_set)} examples")
                 self._test_instance(test_set[0], is_multi_document=ds.is_multi_document, is_dialogue=ds.is_dialogue_based)
+            
+            print_with_color(f"{ds.dataset_name} dataset test complete\n", "32")
 
-        print(f"{'#' * 10} test_all_models {__name__} ENDS {'#' * 10}")
+        print_with_color(f"{'#' * 10} test_all_datasets {__name__} test complete {'#' * 10}", "32")
 
 
 if __name__ == '__main__':
