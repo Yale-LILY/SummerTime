033c6ac871e8fccab4706ee02e33940b8f3f466d
niansong1996 <niansong1996@gmail.com>
2021-09-09 12:32:30 -0400

    change default hmnet weights to pretrained

--- model/dialogue/hmnet_model.py
+++ model/dialogue/hmnet_model.py
@@ -151,10 +151,10 @@ ENT = {
 
 # this is for model pretrained on AMI
 PRETRAINED_MODEL_DOWNLOAD_LINK = (
-    "https://sdrgstorage01wus2.blob.core.windows.net/user"
-    "/ruox/Meeting_Minutes/HMNet/ExampleInitModel/AMI-finetuned/model.pt"
-    "?sv=2019-10-10&st=2020-10-22T19%3A25%3A46Z&se=2060-10-23T19%3A25%3A00Z"
-    "&sr=b&sp=r&sig=VTzk30aQu5KKSgKdW2L9DUYGQyZmns16WnIm%2FifMKZQ%3D"
+    "https://sdrgstorage01wus2.blob.core.windows.net/user/ruox/"
+    "Meeting_Minutes/HMNet/ExampleInitModel/HMNet-pretrained/model.pt"
+    "?sv=2019-10-10&st=2020-10-22T19%3A24%3A06Z&se=2060-10-23T19%3A24%3A00Z"
+    "&sr=b&sp=r&sig=cRfastEaN7s75cgMaBvEFGbXio20smnjjRxxYbqEkoE%3D"
 )
 
 
