56e9e34baaa8b9ae651f42e5fab9e28077faa048
niansong1996 <niansong1996@gmail.com>
2021-11-08 19:03:01 -0500

    change pyrouge back to install from github

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
         "datasets~=1.6.2",
         "sentencepiece~=0.1.95",
         "summ_eval==0.70",
-        "pyrouge",
+        "pyrouge@git+https://github.com/bheinzerling/pyrouge.git",
         "en_core_web_sm@https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.0.0/en_core_web_sm-3.0.0-py3-none-any.whl",
         "jupyter",
         "gdown",
